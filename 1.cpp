ddf82c44cc5cd610313e393f18de0b3ed1c81a7e
hyperjack9888 <79558572+hyperjack9888@users.noreply.github.com>
2021-02-23 21:14:17 -0700

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,141 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	
+	cout<<"Believer"<<endl;
+	cout<<endl;
+	cout<<"First things first"<<endl;
+	cout<<""<<endl;
+	cout<<"I'mma say all the words inside my head"<<endl;
+	cout<<endl;
+	cout<<"I'm fired up and tired of the way that things have been, oh ooh"<<endl;
+	cout<<endl;
+	cout<<"The way that things have been, oh ooh"<<endl;
+	cout<<endl;
+	cout<<endl;
+	cout<<"Second things second"<<endl;
+	cout<<endl;
+	cout<<"Don't you tell me what you think that I could be"<<endl;
+	cout<<endl;
+	cout<<"I'm the one at the sail, I'm the master of my sea, oh ooh"<<endl;	
+	cout<<endl;
+	cout<<"The master of my sea, oh ooh"<<endl;
+	cout<<endl;
+	cout<<endl;
+	cout<<"I was broken from a young age"<<endl;
+	cout<<endl;
+	cout<<"Taking my sulking to the masses"<<endl;
+	cout<<endl;
+	cout<<"Writing my poems for the few"<<endl;
+	cout<<endl;
+	cout<<"That look at me, took to me, shook to me, feeling me"<<endl;
+	cout<<endl;
+	cout<<"Singing from heartache from the pain"<<endl;		
+	cout<<endl;
+	cout<<"Taking my message from the veins"<<endl;
+	cout<<endl;
+	cout<<"Speaking my lesson from the brain"<<endl;
+	cout<<endl;
+	cout<<"Seeing the beauty through the..."<<endl;
+	cout<<endl;
+	cout<<endl;
+	cout<<"Pain!"<<endl;
+	cout<<endl;
+	cout<<"You made me a, you made me a believer, believer"<<endl;
+	cout<<endl;
+	cout<<"Pain!"<<endl;
+	cout<<endl;
+	cout<<"Oh let the bullets fly, oh let them rain"<<endl;
+	cout<<endl;
+	cout<<"My life, my love, my drive, it came from..."<<endl;
+	cout<<endl;
+	cout<<"Pain!"<<endl;
+	cout<<endl;
+	cout<<"You made me a, you made me a believer, believer"<<endl;
+	cout<<endl;
+	cout<<endl;
+	cout<<"Third things third"<<endl;
+	cout<<endl;
+	cout<<"Send a prayer to the ones up above"<<endl;
+	cout<<endl;
+	cout<<"All the hate that you've heard has turned your spirit to a dove, oh ooh"<<endl;
+	cout<<endl;
+	cout<<endl;
+	cout<<endl;
+	cout<<"I was choking in the crowd"<<endl;
+	cout<<endl;
+	cout<<"Building my rain up in the cloud"<<endl;
+    cout<<endl;
+	cout<<"Hoping my feelings, they would drown"<<endl;
+		
+	cout<<"But they never did, ever lived, ebbing and flowing"<<endl;
+		
+	cout<<"Inhibited, limited"<<endl;
+		
+	cout<<"Till it broke open and rained down"<<endl;
+		
+	cout<<"It rained down, like..."<<endl;
+			
+			
+	cout<<"Pain!"<<endl;
+		
+	cout<<"You made me a, you made me a believer, believer"<<endl;
+		
+	cout<<"Pain!"<<endl;
+		
+	cout<<"You break me down, you build me up, believer, believer"<<endl;
+		
+	cout<<"Pain!"<<endl;
+		
+	cout<<"Oh let the bullets fly, oh let them rain"<<endl;
+		
+	cout<<"My life, my love, my drive, it came from..."<<endl;
+		
+	cout<<"Pain!"<<endl;
+		
+	cout<<"You made me a, you made me a believer, believer"<<endl;
+		
+		
+	cout<<"Last things last"<<endl;
+		
+	cout<<"By the grace of the fire and the flames"<<endl;
+		
+	cout<<"You're the face of the future, the blood in my veins, oh ooh"<<endl;
+		
+	cout<<"The blood in my veins, oh ooh"<<endl;
+		
+	cout<<"But they never did, ever lived, ebbing and flowing"<<endl;
+			
+	cout<<"Inhibited, limited"<<endl;
+	cout<<"Till it broke open and rained down"<<endl;
+			
+	cout<<"It rained down, like..."<<endl;
+			
+			
+	cout<<"Pain!"<<endl;
+			
+			
+	cout<<"You made me a, you made me a believer, believer"<<endl;
+			
+	cout<<"Pain!"<<endl;
+			
+	cout<<"You break me down, you build me up, believer, believer"<<endl;
+			
+	cout<<"Pain!"<<endl;
+			
+	cout<<"Oh let the bullets fly, oh let them rain"<<endl;
+			
+	cout<<"My life, my love, my drive, it came from..."<<endl;
+			
+	cout<<"Pain!"<<endl;
+			
+	cout<<"You made me a, you made me a believer, believer"<<endl;
+			
+			
+			
+			
+	cout<<"ow my hands hurt"<<endl;
+	return 0;
+}

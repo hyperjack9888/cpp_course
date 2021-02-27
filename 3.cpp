68ab1b81ac59a2986de81577466f913764123173
hyperjack9888 <79558572+hyperjack9888@users.noreply.github.com>
2021-02-27 08:41:42 -0700

    Add files via upload

---
+++ 3.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	cout<<"Jackie had 9292093 apples he got 387464348738347834738478 amounts of apples muiltyply does two to get the answer! "<<endl;
+	int a =9292093;
+	int b =387464348738347834738478;
+	int result;
+	result =a*b;
+	cout<<"Jackie had "<<endl;
+	cout<<result;
+	cout<<endl;
+	cout<<"apples"<<endl;
+	
+	
+	
+}

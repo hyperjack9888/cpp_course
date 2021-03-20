0b12d01900a2b2718da66bebfcaf2732cf10193b
hyperjack9888 <79558572+hyperjack9888@users.noreply.github.com>
2021-03-20 10:24:00 -0600

    Add files via upload

---
+++ 8.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	string baby,mom,dodo,dad,grandma,grandpa;
+	baby = "Baby shark,";
+	mom = "Mommy shark,";
+	dodo = "doo, doo, doo, doo, doo, doo";
+	dad = "Daddy shark,";
+	grandma = "Grandma shark,";
+	grandpa = "Grandpa shark,"
+	
+	
+	,cout<<baby<<dodo<<endl;
+	cout<<baby<<dodo<<endl;
+	cout<<baby<<dodo<<endl;
+	cout<<baby<<endl;
+	cout<<endl;
+	cout<<mom<<dodo<<endl;
+	cout<<mom<<dodo<<endl;
+	cout<<mom<<dodo<<endl;
+	cout<<mom<<endl;
+	cout<<endl;
+	cout<<dad<<dodo<<endl;
+	cout<<dad<<dodo<<endl;	
+	cout<<dad<<dodo<<endl;	
+	cout<<dad<<endl;
+	cout<<endl;
+	cout<<grandma<<dodo<<endl;
+	cout<<grandma<<dodo<<endl;
+	cout<<grandma<<dodo<<endl;
+	cout<<grandma<<endl;
+	cout<<endl;
+	cout<<grandpa<<dodo<<endl;
+	cout<<grandpa<<dodo<<endl;
+	cout<<grandpa<<dodo<<endl;
+	cout<<grandpa<<endl;
+	
+	return 0;
+                                              }

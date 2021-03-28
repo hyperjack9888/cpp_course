ff1b89dd74ac15b0f63e369aced1364301532341
hyperjack9888 <79558572+hyperjack9888@users.noreply.github.com>
2021-03-28 08:09:04 -0600

    Add files via upload

---
+++ NS.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	
+	int NS;
+	cout<<"Enter A Number: ";
+	cin>>NS;
+	if(0>NS){
+	cout<<"Your number is negative";
+	}else{
+	cout<<"your number is positive";
+	}
+	
+	return 0;
+}

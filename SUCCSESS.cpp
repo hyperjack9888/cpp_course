438bb2ea1049594ba1273e259f98ebe665b62f65
hyperjack9888 <79558572+hyperjack9888@users.noreply.github.com>
2021-03-28 08:32:32 -0600

    Add files via upload

---
+++ SUCCSESS.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+	
+int A;
+
+cout<<"Enter a number: ";
+cin>>A;
+if(A<100){
+	cout<<"IT IS UNDER 100"<<endl;
+}else{
+	cout<<"it is bigger than a 100"<<endl;
+}
+if(A>1000){
+	cout<<"The number you entered is bigger than 1000";
+}else{
+	cout<<"it is under 1000";
+}
+	return 0;
+}

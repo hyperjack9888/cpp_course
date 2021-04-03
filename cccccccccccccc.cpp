9a3ba70d40f6e0ffead0e0bf7045b5cd5a4d1a60
hyperjack9888 <79558572+hyperjack9888@users.noreply.github.com>
2021-04-03 10:22:24 -0600

    Add files via upload

---
+++ cccccccccccccc.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	
+	int A,B,C;
+	cout<<"ENTER A NUMBER: ";	
+//Write a C program to check whether a number is divisible by 5 and 11 or not.
+//Write a C program to check whether a number is even or odd.	
+	cin>>A;
+	if(A%11==0){
+			cout<<"it is divisable by 11 "<<endl;
+	}else{
+		cout<<"it is not divisable by 11"<<endl;
+	}
+	if(A%5==0){
+		cout<<"it is divisable by 5"<<endl;
+	}else{
+		cout<<"it is not diviseable by 5"<<endl;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}

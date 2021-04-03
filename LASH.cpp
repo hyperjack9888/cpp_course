4bb4f79c428d602fb8242bba3d8c091a48044bc6
hyperjack9888 <79558572+hyperjack9888@users.noreply.github.com>
2021-04-03 10:23:17 -0600

    Add files via upload

---
+++ LASH.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	int LASH;
+	cout<<"ENTER A NUMBER: ";
+	cin>>LASH;
+	if(LASH%2==0){
+		cout<<"it is a even number";
+	}else{
+		cout<<"it is a odd number";
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}

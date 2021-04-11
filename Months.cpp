11507f0b7b5c314c39a2a48a71d2cde8872b3b22
hyperjack9888 <79558572+hyperjack9888@users.noreply.github.com>
2021-04-11 11:16:01 -0600

    Add files via upload

---
+++ Months.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	
+	int Month;
+	cout<<"Enter a number: ";
+	cin>>Month;
+	if(Month==1){
+		cout<<"Their are 31 days in the month";
+	}else if(Month==2){
+		cout<<"Their are 29 days in the month";
+	}else if(Month==3){
+		cout<<"Their are 31 days in the month";
+	}else if(Month==4){
+		cout<<"Their are 30 days in the month";
+	}else if(Month==5){
+		cout<<"Their are 31 days in the month";
+	}else if(Month==6){
+		cout<<"Their are 30 days in the month";
+	}else if(Month==7){
+		cout<<"Their are 31 days in the month";
+	}else if(Month==8){
+		cout<<"Their are 31 days in the month";
+	}else if(Month==9){
+		cout<<"Their are 30 days in the month";
+	}else if(Month==10){
+		cout<<"Their are 31 days in the month";
+	}else if(Month==11){
+		cout<<"Their are 30 days in the month";
+	}else if(Month==12){
+		cout<<"Their are 31 days in the month";
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}

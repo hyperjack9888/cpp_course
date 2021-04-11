2227932585c56b09dfea05c89a4190d7e53142f2
hyperjack9888 <79558572+hyperjack9888@users.noreply.github.com>
2021-04-11 10:55:20 -0600

    Add files via upload

---
+++ Days.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	
+	int num1;
+	cout<<"Enter a number: ";
+	cin>>num1;
+	if(num1==1){
+		cout<<"The day is monday";
+	}else if(num1==2){
+		cout<<"The day is tuesday";
+	}else if(num1==3){
+		cout<<"The day is wensday";
+	}else if(num1==4){
+		cout<<"The day is thursday";
+	}else if(num1==5){
+		cout<<"the day is friday";
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
